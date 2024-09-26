d20752bb82a7a80de33fdcaf9f8c6d86cffb6137
Blvckcodeio <ninokayglobal@gmail.com>
2024-09-26 16:43:01 +0100

    Hero Header

--- src/components/Heroheader.js
+++ src/components/Heroheader.js
@@ -1,8 +1,22 @@
+import Image from 'next/image';
+
 const Heroheader = () => {
 
 
     return (
-     <></>
+     <>
+      <div className="hero-container">
+        <div className="cta-container">
+          <h1>DARE TO REBEL</h1>
+          <p>Check out out latest collection now, let’s tune up 
+          your wardrobe</p>
+          <div className="cta-btn">shop now</div>
+        </div>
+        <div className='hero-image'>
+        <Image src="/images/test2.png" alt="Logo" width={911} height={600} />
+        </div>
+      </div>
+     </>
     );
   };
   
